daf74dc9d1258cd4462d1c4cb5befd16aeb1a971
NahuelIglesias <nahueliglesias1999@gmail.com>
2023-03-23 20:24:42 -0300

    Prepared initial scaffolding for application

---
+++ agenda-profesional/src/main/java/com/unqttip/agendaprofesional/AgendaProfesionalApplication.java
@@ -0,0 +1,13 @@
+package com.unqttip.agendaprofesional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaProfesionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaProfesionalApplication.class, args);
+	}
+
+}
---
+++ agenda-profesional/src/test/java/com/unqttip/agendaprofesional/AgendaProfesionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unqttip.agendaprofesional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaProfesionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
