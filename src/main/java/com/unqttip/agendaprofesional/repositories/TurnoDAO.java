a5c5a33725123be44c242962154a087333f7b8df
NahuelIglesias <nahueliglesias1999@gmail.com>
2023-05-02 18:28:52 -0300

    Se agrega validación para distintos turnos

--- src/main/java/com/unqttip/agendaprofesional/repositories/TurnoDAO.java
+++ src/main/java/com/unqttip/agendaprofesional/repositories/TurnoDAO.java
@@ -16,4 +16,10 @@ public interface TurnoDAO extends JpaRepository<Turno, Long> {
 
     @Query(value = "select * from turnos where (horario_inicio between :hora_inicio and :hora_fin) or (horario_fin between :hora_inicio and :hora_fin) order by horario_inicio asc;", nativeQuery = true)
     List<Turno> findWithinHourRange(@Param("hora_inicio") LocalDateTime hora_inicio, @Param("hora_fin") LocalDateTime hora_fin);
+
+    @Query(value = "select count(1) from turnos where tipo in (1,2) and ((:hora_inicio between horario_inicio and horario_fin) or (:hora_fin between horario_inicio and horario_fin));", nativeQuery = true)
+    Integer findCountNonRegularsWithinHourRange(@Param("hora_inicio") LocalDateTime hora_inicio, @Param("hora_fin") LocalDateTime hora_fin);
+
+    @Query(value = "select count(1) from turnos where tipo = 2 and ((:hora_inicio between horario_inicio and horario_fin) or (:hora_fin between horario_inicio and horario_fin));", nativeQuery = true)
+    Integer findCountPrioritariosWithinHourRange(@Param("hora_inicio") LocalDateTime hora_inicio, @Param("hora_fin") LocalDateTime hora_fin);
 }
--- src/main/java/com/unqttip/agendaprofesional/services/TurnoService.java
+++ src/main/java/com/unqttip/agendaprofesional/services/TurnoService.java
@@ -58,7 +58,14 @@ public class TurnoService {
     private void validarTurno(Turno turno) {
         if (turno.getHorarioFin().isBefore(turno.getHorarioInicio())) {
             throw new BadRequestException("Un turno no puede tener una hora final previa a la hora de inicio.");
-        } else if (turnoDAO.findCountInTheSameHour(turno.getHorarioInicio().plusMinutes(1), turno.getHorarioFin().minusMinutes(1)) > 0) {
+        }
+        if (turno.getTipo() == TipoDeTurno.REGULAR && turnoDAO.findCountInTheSameHour(turno.getHorarioInicio().plusMinutes(1), turno.getHorarioFin().minusMinutes(1)) > 0) {
+            throw new BadRequestException("Existen turnos guardados en el horario elegido.");
+        }
+        if (turno.getTipo() == TipoDeTurno.SOBRETURNO && turnoDAO.findCountNonRegularsWithinHourRange(turno.getHorarioInicio().plusMinutes(1), turno.getHorarioFin().minusMinutes(1)) > 0) {
+            throw new BadRequestException("Existen turnos guardados en el horario elegido.");
+        }
+        if (turno.getTipo() == TipoDeTurno.PRIORITARIO && turnoDAO.findCountPrioritariosWithinHourRange(turno.getHorarioInicio().plusMinutes(1), turno.getHorarioFin().minusMinutes(1)) > 0) {
             throw new BadRequestException("Existen turnos guardados en el horario elegido.");
         }
     }
