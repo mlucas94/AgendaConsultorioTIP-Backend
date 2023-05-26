701be96af545baae1c4113c76e8581444c8928fc
NahuelIglesias <nahueliglesias1999@gmail.com>
2023-05-26 14:20:41 -0300

    Se agregan dependencias y se mejora estructura de Profesional.java

--- src/main/java/com/unqttip/agendaprofesional/model/Profesional.java
+++ src/main/java/com/unqttip/agendaprofesional/model/Profesional.java
@@ -7,11 +7,25 @@ import javax.persistence.*;
 public class Profesional {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
     private String email;
 
+    @Column(nullable = false, length = 50)
     private String nombre;
 
+    @Column(nullable = false, length = 64)
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
@@ -35,5 +49,41 @@ public class Profesional {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public static ProfesionalBuilder builder() {
+        return new ProfesionalBuilder();
+    }
+
+    public static final class ProfesionalBuilder {
+        private Profesional profesional;
+
+        private ProfesionalBuilder() {
+            profesional = new Profesional();
+        }
+
+        public ProfesionalBuilder id(Long id) {
+            profesional.setId(id);
+            return this;
+        }
+
+        public ProfesionalBuilder email(String email) {
+            profesional.setEmail(email);
+            return this;
+        }
+
+        public ProfesionalBuilder nombre(String nombre) {
+            profesional.setNombre(nombre);
+            return this;
+        }
+
+        public ProfesionalBuilder password(String password) {
+            profesional.setPassword(password);
+            return this;
+        }
+
+        public Profesional build() {
+            return profesional;
+        }
     }
 }
