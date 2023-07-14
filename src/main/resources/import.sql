b8585d76765dd3458367e81ff45f34fe655e6f39
NahuelIglesias <nahueliglesias1999@gmail.com>
2023-07-14 10:11:33 -0300

    Se actualiza import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,11 +5,11 @@ INSERT INTO pacientes (id, dni, nombre, telefono, edad, email) VALUES (4, 103, '
 INSERT INTO pacientes (id, dni, nombre, telefono, edad, email) VALUES (5, 104, 'Facundo Festa', '1234', '40', 'nahueliglesias1999@gmail.com');
 
 
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (4, '2023-03-09 10:00:00', '2023-03-09 10:30:00', 4, 0, 0);
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (5, '2023-03-09 11:30:00', '2023-03-09 12:00:00', 5, 1, 0);
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (6, '2023-04-09 07:00:00', '2023-04-09 07:30:00', 2, 1, 0);
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (7, '2023-04-09 07:30:00', '2023-04-09 08:00:00', 1, 0, 0);
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (8, '2023-04-09 09:00:00', '2023-04-09 09:30:00', 4, 1, 0);
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (9, '2023-04-09 10:00:00', '2023-04-09 10:30:00', 5, 1, 0);
-INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (10, '2023-04-09 12:00:00', '2023-04-09 13:00:00', 3, 2, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (4, '2023-07-14 10:00:00', '2023-03-09 10:30:00', 4, 0, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (5, '2023-07-14 11:30:00', '2023-03-09 12:00:00', 5, 1, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (6, '2023-07-14 13:00:00', '2023-04-09 07:30:00', 2, 1, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (7, '2023-07-15 09:30:00', '2023-04-09 08:00:00', 1, 0, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (8, '2023-07-15 09:00:00', '2023-04-09 09:30:00', 4, 1, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (9, '2023-07-15 10:00:00', '2023-04-09 10:30:00', 5, 1, 0);
+INSERT INTO turnos (id, horario_inicio, horario_fin, paciente_id, tipo, estado) VALUES (10, '2023-07-16 12:00:00', '2023-04-09 13:00:00', 3, 2, 0);
 
